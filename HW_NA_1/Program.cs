9cc7c81344718e427f74db6d5c50728a2bca06d4
Azanas <azanas.ufa@mail.ru>
2024-06-09 22:59:21 +0300

    hw3

--- HW_NA_1/Program.cs
+++ HW_NA_1/Program.cs
@@ -11,14 +11,16 @@ namespace Server
         static bool exit = false;
         static void Main(string[] args)
         {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
             ThreadPool.QueueUserWorkItem(obj =>
             {
-                Server("Hello");
+                NetServer.Server("Hello", cts);
             });
 
             Console.WriteLine("Для выхода нажмите любую клавишу...");
             Console.ReadKey();
- 
+            cts.Cancel();
         }
 
         static void tasj1()
@@ -29,36 +31,7 @@ namespace Server
             Message? msgDeserialized = Message.DeserializeFromJson(json);
         }
 
-        public static void Server(string message)
-        { 
-            UdpClient udpClient = new UdpClient(12345);
-            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
-           
-            Console.WriteLine("Сервер ждет сообщение от клиента.Нажмите любую клавишу для выхода.");
-            //bool exit = false;
-            while (!exit) 
-                {
-                byte[] buffer = udpClient.Receive(ref iPEndPoint);
-                var messageText = Encoding.UTF8.GetString(buffer);
-
-                ThreadPool.QueueUserWorkItem(obj =>
-                {
-                    Message message1 = Message.DeserializeFromJson(messageText);
-                    message1.Print();
-
-                    // Отправляем подтверждение клиенту
-                    string confirmationMessage = "Сообщение успешно обработано на сервере";
-                    byte[] confirmationBuffer = Encoding.UTF8.GetBytes(confirmationMessage);
-                    udpClient.Send(confirmationBuffer, confirmationBuffer.Length, iPEndPoint);
-                    
-                    if (message1.Text.ToLower() == "exit")
-                    {
-                        exit = true;
-                        Console.WriteLine("Завершение работы...");
-                    }
-                });
-            }
-            udpClient.Close();
-        }
     }
 }
+
+
---
+++ HW_NA_1/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class NetServer
+    {
+
+        //static bool exit = false;
+        public static void Server(string message, CancellationTokenSource cancellationToken)
+        {
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Сервер ждет сообщение от клиента.Нажмите любую клавишу для выхода.");
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    Message message1 = Message.DeserializeFromJson(messageText);
+                    message1.Print();
+
+                    // Отправляем подтверждение клиенту
+                    string confirmationMessage = "Сообщение успешно обработано на сервере";
+                    byte[] confirmationBuffer = Encoding.UTF8.GetBytes(confirmationMessage);
+                    udpClient.Send(confirmationBuffer, confirmationBuffer.Length, iPEndPoint);
+
+                    if (message1.Text.ToLower() == "exit")
+                    {
+                        cancellationToken.Cancel();
+                        Console.WriteLine("Завершение работы...");
+                    }
+                });
+            }
+            udpClient.Close();
+        }
+    }
+}
