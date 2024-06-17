374e613212187c44ab311aa122456058140365eb
Azanas <azanas.ufa@mail.ru>
2024-06-17 20:43:09 +0300

    123123

--- HW_NA_1/Program.cs
+++ HW_NA_1/Program.cs
@@ -1,36 +1,35 @@
-﻿﻿using System;
+﻿using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
-        static bool exit = false;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             CancellationTokenSource cts = new CancellationTokenSource();
 
-            ThreadPool.QueueUserWorkItem(obj =>
-            {
-                NetServer.Server("Hello", cts);
-            });
+            Task task = Task.Run(() => NetServer.Server("Hello", cts));
 
             Console.WriteLine("Для выхода нажмите любую клавишу...");
             Console.ReadKey();
             cts.Cancel();
+
+            await task;
+            await tasj1();
         }
 
-        static void tasj1()
+        static async Task tasj1()
         {
             Message msg = new Message() { Text = "Hello", DateTime = DateTime.Now, NicknameFrom = "Ivan", NicknameTo = "All" };
             string json = msg.SerializeMessageToJson();
             Console.WriteLine(json);
-            Message? msgDeserialized = Message.DeserializeFromJson(json);
+            Message? msgDeserialized = await Task.Run(() => Message.DeserializeFromJson(json));
         }
-
     }
 }
 
